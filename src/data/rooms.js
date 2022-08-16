838cd696601c8e978377de0e1649a8fb0673f58e
TheChiran <tonmoychiran449@gmail.com>
2022-08-16 15:24:12 +0600

    [Task-> 1. Fixed readme file image issue]

--- src/data/rooms.js
+++ src/data/rooms.js
@@ -1,7 +1,7 @@
 const rooms = [
   {
     id: 1,
-    imgUrl: "./src/assets/images/dynamic/room-1.jpg",
+    imgUrl: "/assets/images/dynamic/room-1.jpg",
     label: "Super Host",
     title: "Entire apartment . 2 beds",
     description: "Stylist apartment in center of the city",
@@ -9,21 +9,21 @@ const rooms = [
   },
   {
     id: 2,
-    imgUrl: "./src/assets/images/dynamic/room-2.jpg",
+    imgUrl: "/assets/images/dynamic/room-2.jpg",
     title: "Private room",
     description: "Cozy, peaceful and private room with...",
     rating: 4.25,
   },
   {
     id: 3,
-    imgUrl: "./src/assets/images/dynamic/room-3.jpg",
+    imgUrl: "/assets/images/dynamic/room-3.jpg",
     title: "Entire house",
     description: "Mordern House in a remote area",
     rating: 4.96,
   },
   {
     id: 4,
-    imgUrl: "./src/assets/images/dynamic/room-4.jpg",
+    imgUrl: "/assets/images/dynamic/room-4.jpg",
     label: "Super Host",
     title: "Entire apartment . 2 beds",
     description: "Stylist room in design district ",
@@ -31,14 +31,14 @@ const rooms = [
   },
   {
     id: 5,
-    imgUrl: "./src/assets/images/dynamic/room-5.jpg",
+    imgUrl: "/assets/images/dynamic/room-5.jpg",
     title: "Entire house",
     description: "House in a remote area",
     rating: 4.64,
   },
   {
     id: 6,
-    imgUrl: "./src/assets/images/dynamic/room-6.jpg",
+    imgUrl: "/assets/images/dynamic/room-6.jpg",
     title: "Private room",
     description: "Cozy, peaceful and private room with...",
     rating: 4.25,
